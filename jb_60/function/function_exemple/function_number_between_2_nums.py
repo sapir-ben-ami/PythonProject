8ae038a7e136aeca9dc668967b8f4a250e325ef7
sapir <sapir.benami@mail.huji.ac.il>
2025-08-11 13:59:43 +0300

    FIRST COMMIT

---
+++ jb_60/function/function_exemple/function_number_between_2_nums.py
@@ -0,0 +1,8 @@
+from jb_60.function.utils import utils
+
+utils_instance = utils()
+num1 = 10
+num2 = 3
+
+utils_instance.print_num_between_2_nums(num1, num2)
+utils_instance.print_num_between_2_nums(num2, num1)
